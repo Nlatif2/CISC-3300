1e21b21c880b0a30b87b0b14ff3a993d9580e54e
Nafisa Latif <nafisalatif@Nafisas-MacBook-Air.local>
2024-11-06 20:21:05 -0500

    Removed folders that had nothing in them

--- homework8-6.php
+++ homework8-6.php
@@ -1,20 +1,13 @@
 <?php
 
 try {
-    // Create an expectation by checking for a specific condition
     $number = 10;
-
-    // Let's say we expect $number to be greater than 20
     if ($number <= 20) {
-        // If this condition is met, throw an exception
         throw new Exception("The number must be greater than 20. Given: $number");
     }
-
-    // If the condition is met, this line will not execute
     echo "The number is valid.";
 
 } catch (Exception $e) {
-    // Custom message in the catch block
     echo "Error: " . $e->getMessage();
 }
 
